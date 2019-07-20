ada4fb8848f05b8a529a985c1ca9fa4165bdb5d1
JOhnBreaker <38775739JOhnBreaker@users.noreply.github.com>
2019-07-20 16:23:59 +0800

    把整个项目上传上去了

---
+++ 13.2/cmake-build-debug/CMakeFiles/ClionProject.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionProject.dir/main.cpp.obj"
+  "CMakeFiles/ClionProject.dir/Cd.cpp.obj"
+  "CMakeFiles/ClionProject.dir/Classic.cpp.obj"
+  "ClionProject.pdb"
+  "ClionProject.exe"
+  "ClionProject.exe.manifest"
+  "libClionProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
